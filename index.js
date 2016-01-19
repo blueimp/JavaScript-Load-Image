e2544488cba1cb2068c026c534fb66c8857e1bc3
ruscoder <rusmiligamer@gmail.com>
2016-01-19 16:25:58 +0700

    Changed paths for define

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 module.exports = require('./js/load-image');
+
+require('./js/load-image-exif');
+require('./js/load-image-exif-map');
+require('./js/load-image-ios');
+require('./js/load-image-meta');
+require('./js/load-image-orientation');
