3d77fe50aab51c3127787631250d5e011a9c23dc
Joel <joel.lavoie.ing@gmail.com>
2016-04-22 11:28:56 -0400

    Fix for avoid build issue for browser
    verify if the code is to build for nodeJs or for the browser. If the library is built and minified for the browser, the NodeJS code in index.js will not be added because module.exports cannot interpreted by the browser.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+if (typeof module === 'object' && module.exports) {
 module.exports = require('./js/load-image')
 
 require('./js/load-image-exif')
 require('./js/load-image-exif-map')
 require('./js/load-image-meta')
 require('./js/load-image-orientation')
+}
