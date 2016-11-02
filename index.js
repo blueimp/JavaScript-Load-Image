33b1317e83de49ccfc84604b1be449109600473b
Sebastian Tschan <mail@blueimp.net>
2016-11-02 22:41:14 +0100

    Move index.js into js directory.

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = require('./js/load-image')
-
-require('./js/load-image-exif')
-require('./js/load-image-exif-map')
-require('./js/load-image-meta')
-require('./js/load-image-orientation')
